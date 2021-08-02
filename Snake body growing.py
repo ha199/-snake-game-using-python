c5f0972d5d4f02c7fb6730406d2b3d81887381ee
Hari Singh Meena <76901890+ha199@users.noreply.github.com>
2021-08-02 17:49:29 +0530

    Create Snake body growing.py

---
+++ Snake body growing.py
@@ -0,0 +1,9 @@
+
+
+    # Snake body growing mechanism
+    snake_body.insert(0, list(snake_pos))
+    if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
