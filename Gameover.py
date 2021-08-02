405a74643739b652b20423eeaf63c59fbea1530e
Hari Singh Meena <76901890+ha199@users.noreply.github.com>
2021-08-02 17:44:05 +0530

    Create Gameover.py

---
+++ Gameover.py
@@ -0,0 +1,16 @@
+
+
+
+# Game Over
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('YOU DIED', True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    show_score(0, red, 'times', 20)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
