ccb6454c8ff058c62e756aa4556f6d1f7f9cbab1
Hari Singh Meena <76901890+ha199@users.noreply.github.com>
2021-08-02 17:46:43 +0530

    Create Score.py

---
+++ Score.py
@@ -0,0 +1,11 @@
+# Score
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (frame_size_x/10, 15)
+    else:
+        score_rect.midtop = (frame_size_x/2, frame_size_y/1.25)
+    game_window.blit(score_surface, score_rect)
+    # pygame.display.flip()
