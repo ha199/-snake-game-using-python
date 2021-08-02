af78de47a03a9b2fa4c6a88936220171373c572f
Hari Singh Meena <76901890+ha199@users.noreply.github.com>
2021-08-02 17:48:12 +0530

    Create Mainlogic.py

---
+++ Mainlogic.py
@@ -0,0 +1,41 @@
+
+# Main logic
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        # Whenever a key is pressed down
+        elif event.type == pygame.KEYDOWN:
+            # T -> Up; V -> Down; D -> Left; H -> Right
+            if event.key == pygame.K_UP or event.key == ord('t'):
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('v'):
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT or event.key == ord('d'):
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT or event.key == ord('h'):
+                change_to = 'RIGHT'
+            # Esc -> Create event to quit the game
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+    # Making sure the snake cannot move in the opposite direction instantaneously
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+    # Moving the snake
+    if direction == 'UP':
+        snake_pos[1] -= 10
+    if direction == 'DOWN':
+        snake_pos[1] += 10
+    if direction == 'LEFT':
+        snake_pos[0] -= 10
+    if direction == 'RIGHT':
+        snake_pos[0] += 10
